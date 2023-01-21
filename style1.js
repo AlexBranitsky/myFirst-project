c4814a4f02ba1925f9020b71b4369e0f861b57df
Alexander Branitski <alexbranitsky1983@icloud.com>
2023-01-21 14:36:10 +0300

    first commit

--- style1.js
+++ style1.js
@@ -1,56 +1,62 @@
 'use strict';
-const btnAdd = document.querySelector('.btnadd');
-btnAdd.addEventListener('click', addNewUser);
-function addNewUser () {
-    // создание новой строки
+// извлечение кнопки
+const NewUser = document.querySelector('.btnadd');
+// кликабельность кнопки
+NewUser.addEventListener('click', newUser);
+// функция для запуска полей ввода
+function newUser () {
+    // клонирование новой строки
     const template = document.querySelector('.newrow');
     const newrow = template.content.cloneNode(true);
     const tbody = document.querySelector('.myTable');
-
     const tr = newrow.querySelector('tr');
     const nameInput = newrow.querySelector('.name input');
     const emailInput = newrow.querySelector('.email input');
     const cityInput = newrow.querySelector('.city input');
     const telephoneInput = newrow.querySelector('.telephone input');
-
+    // добавление строки в таблицу
     tbody.append(tr);
-
-    // btn cancel
+    // извлечение кнопки отмена
     const btnCancel = document.querySelector('.btncancel');
+    // кликабельность кнопки и функия
     btnCancel.addEventListener('click', cancel);
+    // функция для отмены введенного текста
     function cancel () {
         tbody.removeChild(tr);
     }
-    // btn save
-    const btnSave = document.querySelector('.btnsave');
-    btnSave.addEventListener('click', saveUser);
-    function saveUser () {
+    // извлечение кнопки сохранить
+    const saveUser = document.querySelector('.btnsave');
+    // кликабельность кнопки /функция
+    saveUser.addEventListener('click', save);
+    // функция для сохранения
+    function save () {
         const template = document.querySelector('.row1');
         const row1 = template.content.cloneNode(true);
         const tbody = document.querySelector('.myTable');
-
+        // сохранение данных
         row1.querySelector('.name').innerText = nameInput.value;
         row1.querySelector('.email').innerText = emailInput.value;
         row1.querySelector('.city').innerText = cityInput.value;
         row1.querySelector('.telephone').innerText = telephoneInput.value;
-        
+
+        // извлечение кнопки для удаления введеной строки
         const tr2 = row1.querySelector('tr');
-        row1.querySelector('.deleteInfo').addEventListener('click', () => {
+        const userDel = row1.querySelector('.deleteInfo');
+        // функция для удаления строки
+        userDel.addEventListener('click', delUser);
+        function delUser () {
             tbody.removeChild(tr2);
-        });
+        };
 
         tbody.append(row1);
         tbody.removeChild(tr);
-    };
-    
+    }
 }
-const btnDelUsers = document.querySelector('.btndelete');
-btnDelUsers.addEventListener('click', deleteRowsFromBody);
-function deleteRowsFromBody () {
+const delRowsFromBody = document.querySelector('.btndelete');
+delRowsFromBody.addEventListener('click', delRows);
+function delRows () {
     const tbody = document.querySelector('.myTable');
     console.log('tbody', tbody);
     const allRows = tbody.querySelectorAll('tr');
     allRows.forEach((element) => tbody.removeChild(element));
 }
-
-
